ddb79070fb08f830f26686454ab8678a06779397
Sergei M <black_white2018@rambler.ru>
2020-06-12 21:45:13 +0300

    Updates

---
+++ precache-manifest.2ce0face54da17153bf45d53da56aa1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa6211ccd9e8f66b51d3a48eb3e09ea",
+    "url": "/training-records/index.html"
+  },
+  {
+    "revision": "d2c3f9073e9bcd1f8504",
+    "url": "/training-records/static/css/main.1118090d.chunk.css"
+  },
+  {
+    "revision": "b4c541af40c3bee53770",
+    "url": "/training-records/static/js/2.cce414c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/training-records/static/js/2.cce414c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c3f9073e9bcd1f8504",
+    "url": "/training-records/static/js/main.f51c6870.chunk.js"
+  },
+  {
+    "revision": "831371b4f21a237fb23e",
+    "url": "/training-records/static/js/runtime-main.668df92e.js"
+  }
+]);
